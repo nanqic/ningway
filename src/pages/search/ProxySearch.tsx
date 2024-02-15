4e9d767da07836503856cec8c6b36e991f224cf5
nanqic <nanqic@163.com>
2024-02-15 10:44:41 +0800

    opt: vsearch onload

--- src/pages/search/ProxySearch.tsx
+++ src/pages/search/ProxySearch.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
@@ -6,23 +6,28 @@ import { useParams } from 'react-router-dom';
 export default function ProxySearch() {
   const [src, setSrc] = useState<string>('')
   let { keywords } = useParams();
+  const [message, setMessage] = useState('加载中...')
 
   useEffect(() => {
     if (keywords && keywords != '') {
-      setSrc(`${import.meta.env.VITE_PROXY_URL}${btoa(encodeURI('/'+keywords))}`)
+      setSrc(`${import.meta.env.VITE_PROXY_URL}${btoa(encodeURI('/' + keywords))}`)
     }
   }, [])
 
-
-
   return (
     <Box height={'100vh'}>
+      {message && <Typography variant='h5' margin={1.5}>
+        {message}
+      </Typography>}
       {src && <iframe style={{
         border: 'none',
         height: '100%',
         width: '100%',
       }}
-        src={src} />}
+        src={src}
+        onLoad={() => setMessage('')}
+        onError={() => setMessage('出错了~')}
+      />}
     </Box>
   )
 }
