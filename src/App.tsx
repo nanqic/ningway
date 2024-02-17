a8b94197b6f84ce9eb877b6a603ff7c7c63c884b
nanqic <nanqic@163.com>
2024-02-17 13:11:56 +0800

    fix: qa

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
     useEffect(() => {
         let timer = setTimeout(() => {
             const readme = localStorage.getItem(import.meta.env.VITE_README)
-            if (readme == undefined) location.replace('/help')
+            if (readme == undefined) location.replace('/about')
             localStorage.setItem(import.meta.env.VITE_README, 'true')
         }, 1000 * 60)
 
