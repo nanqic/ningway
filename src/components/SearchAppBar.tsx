0123d774a534fb4fe874fa2a82ae8617d8c7252e
nanqic <nanqic@163.com>
2024-02-26 19:31:37 +0800

    opt: menu padding

--- src/components/SearchAppBar.tsx
+++ src/components/SearchAppBar.tsx
@@ -132,7 +132,6 @@ export default function SearchAppBar() {
                     component="a"
                     sx={{
                         cursor: "pointer",
-                        mr: 1,
                         display: { xs: "none", md: "flex" },
                         fontFamily: "monospace",
                         fontWeight: 700,
@@ -148,11 +147,10 @@ export default function SearchAppBar() {
                 <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
                     <IconButton
                         size="large"
-                        aria-label="account of current user"
                         aria-controls="menu-appbar"
                         aria-haspopup="true"
+                        sx={{ pl: 0 }}
                         onClick={handleOpenNavMenu}
-                        color="inherit"
                     >
                         <MenuIcon />
                     </IconButton>
