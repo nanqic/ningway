4d68373f433c110b0ae2c9a1673a7bc363cc9f59
nanqic <nanqic@163.com>
2024-03-05 15:36:01 +0800

    opt : aysnc post

--- src/pages/search/ProxySearch.tsx
+++ src/pages/search/ProxySearch.tsx
@@ -1,5 +1,5 @@
 import { searchHead } from '@/store/template';
-import { postSearchData } from '@/utils/requestUtil';
+import { postKeywords, postSearchData } from '@/utils/requestUtil';
 import { Box, Typography } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
@@ -13,35 +13,36 @@ export default function ProxySearch() {
   const [searchParams, _] = useSearchParams()
   const page = searchParams.get('page')
   const navigate = useNavigate()
-  const fetchHtml = (iframeUrl: string) => {
-    fetch(iframeUrl)
-      .then(resp => {
-        const statusCode = resp.status; // 获取响应的状态码
-        if (statusCode == 200) {
-          return resp.text()
-        }
 
-        console.info('外部iframe的状态码：', statusCode);
-      })
-      .then(text => {
-        if (text?.includes("服务")) {
-          setMessage('服务繁忙，可以先搜索缓存')
-          return setTimeout(() => navigate('/tool/5')
-            , 3000)
-        }
+  const fetchHtml = async (iframeUrl: string) => {
+    const resp = await fetch(iframeUrl)
 
-        if (text) {
-          setSrc(searchHead + text)
-          keywords && text.includes("个视频") && postSearchData(keywords + (page ? '_p' + page : ''), text)
-          setMessage(' ')
-        }
-      })
-      .catch(function (error) {
-        // console.info('请求外部iframe时发生错误：', error);
-        setMessage('服务器出错了，可以先搜索缓存')
-        return setTimeout(() => navigate('/tool/5')
-          , 3000)
-      })
+    const statusCode = resp.status; // 获取响应的状态码
+    if (statusCode !== 200) {
+      console.info('外部iframe的状态码：', statusCode);
+      setMessage('服务器出错了，可以先搜索缓存')
+      return setTimeout(() => navigate('/tool/5')
+        , 3000)
+    }
+
+    const text = await resp.text()
+
+    if (text?.includes("服务")) {
+      setMessage('服务繁忙，可以先搜索缓存')
+      return setTimeout(() => navigate('/tool/5')
+        , 3000)
+    }
+
+    if (text) {
+      setSrc(searchHead + text)
+      setMessage(' ')
+
+      if (keywords && text.includes("个视频")) {
+        // const isOk = await postKeywords({ keywords, comment: text })
+        // isOk && 
+        postSearchData(keywords + (page ? '_p' + page : ''), text)
+      }
+    }
   }
 
   useEffect(() => {
--- src/utils/requestUtil.ts
+++ src/utils/requestUtil.ts
@@ -1,4 +1,4 @@
-import { getVsearchCount } from "./dbUtil";
+import { SearchItem, getVsearchCount } from "./dbUtil";
 
 export async function getUri(uri: string) {
     return await (await fetch(`/api/${uri}`)).json()
@@ -16,10 +16,10 @@ export const fetchPageview = async () => {
     }
 }
 
-export async function postSearchData(keywords: string, body: string) {
+export async function postSearchData(keywords: string, comment: string) {
     const url = import.meta.env.VITE_WL_SERVER + 'api/comment/202cb962';
     const data = {
-        comment: body,
+        comment: comment,
         nick: 'search_' + keywords,
         url: '/202cb962'
     };
@@ -60,6 +60,44 @@ export async function postCountData() {
 // &sortBy=insertedAt_asc 按插入顺序
 export const getHotSearch = async (page = 1, pageSize = 100) => {
     const url = `${import.meta.env.VITE_WL_SERVER}api/comment?path=/202cb962&pageSize=${pageSize}&page=${page}&sortBy=insertedAt_asc`;
+
+    const response = await fetch(url)
+    if (response.ok) {
+        const result = await response.json();
+        // console.log(result);
+        return result.data
+    }
+
+    console.error('Error:', response.status);
+    return response.status
+}
+
+export const postKeywords = async (searchItem: SearchItem) => {
+    const url = `https://search-key.ningway.com/api`;
+    const data = {
+        comment: searchItem.comment,
+        keywords: searchItem.keywords,
+        secret: '10cc202cb962'
+    };
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    if (response.ok) {
+        const result = await response.json();
+        console.log(result);
+        return result.ok
+    }
+
+    console.error('Error:', response.status);
+    return response.status
+}
+export const getKeywords = async (keywords: string) => {
+    const url = `https://search-key.ningway.com/api`;
 
     const response = await fetch(url)
     if (response.ok) {
