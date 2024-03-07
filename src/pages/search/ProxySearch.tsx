5d8c4e6d1a85985c2b6efdd780a1dda2ddc99cd0
nanqic <nanqic@163.com>
2024-03-07 18:26:33 +0800

    fix: next page

--- src/pages/search/ProxySearch.tsx
+++ src/pages/search/ProxySearch.tsx
@@ -8,32 +8,36 @@ import { countVsearch } from '@/utils/dbUtil';
 
 export default function ProxySearch() {
   const [src, setSrc] = useState<string>()
-  const { keywords } = useParams();
+  let { keywords } = useParams();
   const [message, setMessage] = useState<string>("")
   const [searchParams, _] = useSearchParams()
   const page = searchParams.get('page')
   const { state } = useLocation();
   const navigate = useNavigate()
+  let url = searchParams.get('url')
 
-  if (!keywords || keywords.trim().length == 0) {
-    return <></>
+  if (keywords?.trim().length == 0 || url?.trim().length == 0
+  ) {
+    return <>路由未解析 {keywords}{url}</>
   }
 
-  const fetchHtml = async () => {
+  const fetchHtml = async (keywords: string) => {
     // state true ：搜索过关键字且没有缓存
     if (!state) {
-      const res = await fetchComment(keywords + (page ? '_p' + page : ''))
-      if (res?.comment != '') {
+      const res = await fetchComment(keywords)
+      // 已定义且不为空
+      if (res?.comment && res.comment != '') {
         return navigate(`/cache/${keywords}#unique`, { state: res.comment })
       } else if (res?.comment === '') {
         return navigate(`/cache/${keywords}#unique`, { state: "没有视频符合搜索的条件。" })
       }
     }
 
-    const serverUrl = `${import.meta.env.VITE_PROXY_URL}${btoa(encodeURI('/' + keywords) + (page ? '?page=' + page : ''))}`
+    const serverUrl = `${import.meta.env.VITE_PROXY_URL}${btoa(encodeURI('/' + keywords.split('_p')[0]) + (page ? '?page=' + page : ''))}`
     setMessage('请求已发送，等待服务器响应...')
-    
+
     const resp = await fetch(serverUrl)
+    console.log(resp);
 
     const statusCode = resp.status; // 获取响应的状态码
     if (statusCode !== 200) {
@@ -54,7 +58,7 @@ export default function ProxySearch() {
     setMessage(' ')
     if (!text.includes('DOCTYPE')) {
       // 没有搜到视频也存键和空值
-      postKeywords(keywords + (page ? '_p' + page : ''), text)
+      postKeywords(keywords, text)
     }
   }
 
@@ -70,17 +74,19 @@ export default function ProxySearch() {
     }
 
     // 处理点击下一页的搜索
-    let url = searchParams.get('url')
     if (url) {
-      const subtitle = decodeURI(atob(url || '').slice(37).split('?page=')[0])
       const pagePram = atob(url || '').split('page=')[1]
-      if (subtitle !== '' && pagePram !== null) {
-        navigate(`/vsearch/${subtitle}?page=${pagePram}`)
+      keywords = decodeURI(atob(url || '').slice(37).split('?page=')[0]) + (pagePram ? '_p' + pagePram : '')
+
+      if (keywords !== '' && pagePram !== null) {
+        navigate(`/vsearch/${keywords}?page=${pagePram}`)
       }
     }
 
-    fetchHtml()
-    countVsearch(keywords)
+    if (keywords) {
+      fetchHtml(keywords)
+      countVsearch(keywords)
+    }
   }, [searchParams])
 
   return (
