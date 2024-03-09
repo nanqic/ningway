d7c4defff2614654c6fc4fb9cf9f852bb95d499d
nanqic <nanqic@163.com>
2024-03-09 11:54:41 +0800

    opt: copy util

--- src/utils/clipboard-util.js
+++ src/utils/clipboard-util.js
@@ -1,15 +1,11 @@
 export async function copyTextToClipboard(text) {
     let copyStatus;
-    if (navigator.clipboard) {
-        try {
-            await navigator.clipboard.writeText(text);
-            console.log('文本已成功复制到剪贴板');
-            copyStatus = true
-        } catch (error) {
-            console.error('复制到剪贴板时出现错误:', error);
-            copyStatus = false
-        }
-    } else {
+    try {
+        await navigator.clipboard.writeText(text);
+        console.log('文本已成功复制到剪贴板');
+        copyStatus = true
+    } catch (error) {
+        console.error('复制到剪贴板时出现错误:', error);
         copyStatus = copy2(text)
     }
     console.log('Text copied to clipboard successfully.', text, copyStatus);
@@ -19,18 +15,22 @@ export async function copyTextToClipboard(text) {
 
 function copy2(text) {
     let copyStatus;
-    if ((navigator.userAgent.match(/(iPhone|iPod|iPad);?/i))) {
+    if (navigator.userAgent.match(/(iPhone|iPod|iPad);?/i)) {
         const range = document.createRange();
-        range.selectNode(document.querySelector("#copyTxt"));
+        const input = document.createElement("input");
+        input.value = text;
+        document.body.appendChild(input);
+        range.selectNode(input);
         const selection = window.getSelection();
         if (selection.rangeCount > 0) selection.removeAllRanges();
         selection.addRange(range);
         copyStatus = document.execCommand('copy');
         selection.removeAllRanges();
+        document.body.removeChild(input);
+        console.log('ios copyed:', text);
     } else {
-        const input = document.createElement("INPUT");
+        const input = document.createElement("input");
         input.value = text;
-        input.className = "";
         document.body.appendChild(input);
         input.select();
         copyStatus = document.execCommand('copy')
