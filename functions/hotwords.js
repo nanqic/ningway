f2a59ece4007ed6c38614fb6fbbe9036d0f66f75
nanqic <nanqic@163.com>
2024-03-21 10:11:02 +0800

    opt: funcs

--- functions/hotwords.js
+++ functions/hotwords.js
@@ -6,18 +6,17 @@ const headers = {
 
 export default {
     async fetch(request, env) {
-        const url = new URL(request.url);
-        if (url.pathname === '/apis/hot') {
+        if (request.method == 'POST') {
             let siteurl = `https://ziguijia.com/search`
             const res = await fetch(siteurl)
             let text = await res.text()
-            
+
             const regx = new RegExp(`<(script|style|footer|button)(.|\n)*?>(.|\n)*?</(script|style|footer|button)>|<!DOC(.|\n)*?<(hr/?)>`)
             text = text.replace(regx, '')
 
             let pattern = /<a.*?>(.*?)<\/a>/g;
             let match, words = [];
-        
+
             while (match = pattern.exec(text)) {
                 words.push(match[1]); // 匹配到的<a>标签内的内容
             }
