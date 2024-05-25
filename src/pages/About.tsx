5e5bc6d0d1fb270574199e67aef8b05bf38fbe77
Amigo <nanqic@163.com>
2024-05-25 21:46:03 +0800

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -32,7 +32,6 @@ export default function About() {
                 <li>
                     网站业余维护，反馈回复会有延迟，请您谅解。
                 </li>
-                <a href='/donate'>捐赠支持</a>
             </ul>
             <Typography component={"li"} variant='subtitle2'>邮箱<OutLink href="mailto:contact@ningway.com"> contact@ningway.com </OutLink></Typography>
 
@@ -46,6 +45,8 @@ export default function About() {
                 <OutLink href="www.fahaisouxun.com/search">法海搜寻 </OutLink>
                 <OutLink href="www.huidengchanxiu.net/books/b1">禅修教材 </OutLink>
             </Typography>
+            <br/>
+            <Link href='/donate'>donate</Link>
         </Container>
     )
 }
