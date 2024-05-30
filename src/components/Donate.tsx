11376bb741dc0064b6a71b96c27760cecca95edf
nanqic <nanqic@163.com>
2024-05-30 14:49:39 +0800

    添加 无关说明

--- src/components/Donate.tsx
+++ src/components/Donate.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Button, Link, Typography } from '@mui/material'
 import NotFound from './NotFound'
 import QRcodBaseUA from './QRcodBaseUA'
 import { getPlaystatSize } from '@/utils/dbUtil'
@@ -20,9 +20,10 @@ export default function Donate() {
                 勤劳如山王，不如积微福。<br />
             </Typography>
             <QRcodBaseUA />
-            <h5>
+            <h4>
                 请长按二维码保存，再用微信/支付宝扫一扫
-            </h5>
+            </h4>
+            <i><h5> 说明：此网站与任何组织无关，详情查看 <Link href='/about'>关于本站</Link></h5></i>
             <Typography sx={{ mt: 5 }} variant="subtitle2">
                 愿访此网人，悉发菩提心。
             </Typography>
