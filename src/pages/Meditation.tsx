e97b3de8c2c9cf2531b7fefba102a3996b90eb8f
nanqic <nanqic@163.com>
2024-06-01 11:30:25 +0800

    opt: zen muisc

--- src/pages/Meditation.tsx
+++ src/pages/Meditation.tsx
@@ -9,27 +9,28 @@ const jingzuo = [{ title: "（一）基 础", codes: ["40588", "21386", "40518",
 
 const Muisc = () => {
   const playlist = [
-    { name: '养心静坐 45 分钟', src: 'https://download.ziguijia.com/etc/%E5%85%BB%E5%BF%83%E9%9D%99%E5%9D%90_192k.mp3?name=%E5%85%BB%E5%BF%83%E9%9D%99%E5%9D%90_192k.mp3' },
-    { name: '海潮音 60 分钟', src: 'https://download.ziguijia.com/etc/%E8%80%B3%E6%A0%B9%E5%9C%86%E9%80%9A%EF%BC%88%E6%B5%B7%E6%BD%AE%E9%9F%B3%EF%BC%89_192k.mp3?name=%E8%80%B3%E6%A0%B9%E5%9C%86%E9%80%9A%EF%BC%88%E6%B5%B7%E6%BD%AE%E9%9F%B3%EF%BC%89_192k.mp3' },
-    { name: '名号 120 分钟', src: 'https://download.ziguijia.com/etc/%E8%80%B3%E6%A0%B9%E5%9C%86%E9%80%9A%EF%BC%88%E8%A7%82%E4%B8%96%E9%9F%B3%E5%90%8D%E5%8F%B7%EF%BC%89_192k.mp3?name=%E8%80%B3%E6%A0%B9%E5%9C%86%E9%80%9A%EF%BC%88%E8%A7%82%E4%B8%96%E9%9F%B3%E5%90%8D%E5%8F%B7%EF%BC%89_192k.mp3' },
-    { name: '清洗 180 分钟', src: 'https://download.ziguijia.com/etc/%E8%84%89%E8%BD%AE%E6%B8%85%E6%B4%97_192k.mp3?name=%E8%84%89%E8%BD%AE%E6%B8%85%E6%B4%97_192k.mp3' },
-    { name: '海潮音 180 分钟', src: 'https://download.ziguijia.com/etc/%E8%80%B3%E6%A0%B9%E5%9C%86%E9%80%9A%EF%BC%88%E6%B5%B7%E6%BD%AE%E9%9F%B3%203%E5%B0%8F%E6%97%B6%E7%89%88%EF%BC%89_192k.mp3?name=%E8%80%B3%E6%A0%B9%E5%9C%86%E9%80%9A%EF%BC%88%E6%B5%B7%E6%BD%AE%E9%9F%B3%203%E5%B0%8F%E6%97%B6%E7%89%88%EF%BC%89_192k.mp3' },
-    { name: '太极桩 56 分钟', src: 'https://download.ziguijia.com/etc/%E5%A4%AA%E6%9E%81%E6%A1%A9_192k.mp3?name=%E5%A4%AA%E6%9E%81%E6%A1%A9_192k.mp3' },
+    { name: '养心静坐 45 分钟', src: '%E5%85%BB%E5%BF%83%E9%9D%99%E5%9D%90_192k.mp3?name=%E5%85%BB%E5%BF%83%E9%9D%99%E5%9D%90_192k.mp3' },
+    { name: '海潮音 60 分钟', src: '%E8%80%B3%E6%A0%B9%E5%9C%86%E9%80%9A%EF%BC%88%E6%B5%B7%E6%BD%AE%E9%9F%B3%EF%BC%89_192k.mp3?name=%E8%80%B3%E6%A0%B9%E5%9C%86%E9%80%9A%EF%BC%88%E6%B5%B7%E6%BD%AE%E9%9F%B3%EF%BC%89_192k.mp3' },
+    { name: '名号 120 分钟', src: '%E8%80%B3%E6%A0%B9%E5%9C%86%E9%80%9A%EF%BC%88%E8%A7%82%E4%B8%96%E9%9F%B3%E5%90%8D%E5%8F%B7%EF%BC%89_192k.mp3?name=%E8%80%B3%E6%A0%B9%E5%9C%86%E9%80%9A%EF%BC%88%E8%A7%82%E4%B8%96%E9%9F%B3%E5%90%8D%E5%8F%B7%EF%BC%89_192k.mp3' },
+    { name: '清洗 180 分钟', src: '%E8%84%89%E8%BD%AE%E6%B8%85%E6%B4%97_192k.mp3?name=%E8%84%89%E8%BD%AE%E6%B8%85%E6%B4%97_192k.mp3' },
+    { name: '海潮音 180 分钟', src: '%E8%80%B3%E6%A0%B9%E5%9C%86%E9%80%9A%EF%BC%88%E6%B5%B7%E6%BD%AE%E9%9F%B3%203%E5%B0%8F%E6%97%B6%E7%89%88%EF%BC%89_192k.mp3?name=%E8%80%B3%E6%A0%B9%E5%9C%86%E9%80%9A%EF%BC%88%E6%B5%B7%E6%BD%AE%E9%9F%B3%203%E5%B0%8F%E6%97%B6%E7%89%88%EF%BC%89_192k.mp3' },
+    { name: '太极桩 56 分钟', src: '%E5%A4%AA%E6%9E%81%E6%A1%A9_192k.mp3?name=%E5%A4%AA%E6%9E%81%E6%A1%A9_192k.mp3' },
   ]
   const [src, setSrc] = useState('')
   return <>
-    <audio
-      style={{ width: '100%',margin: '1rem'}}
-      controls
-      autoPlay
-      controlsList="nodownload"
-      src={src}></audio>
+    {src &&
+      <audio
+        style={{ width: '100%', margin: '1rem' }}
+        controls
+        autoPlay
+        controlsList="nodownload"
+        src={`https://download.ziguijia.com/etc/${src}`}></audio>}
     {playlist.map(item => {
       return <Button key={item.src} startIcon={<PlayCircleIcon />}
         onClick={() => setSrc(item.src)}
         sx={{
           display: 'flex',
-          color: item.src===src? 'green': ''
+          color: item.src === src ? 'green' : ''
         }}>{item.name}</Button>
 
     })}
