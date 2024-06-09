b39dab9f744599794d9d66f189d0a81ad378f646
nanqic <nanqic@163.com>
2024-06-09 12:11:13 +0800

    fix: md hide bottom nav

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -22,7 +22,7 @@ export default function BottomNav() {
     };
 
     return (
-        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, display:{ md: "none"} }} elevation={3}>
             <BottomNavigation showLabels value={location.pathname.slice(1) || value} onChange={handleChange}>
                 <BottomNavigationAction
                     label="列表"
